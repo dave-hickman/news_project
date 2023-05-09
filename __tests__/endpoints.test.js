3ced5be280f28f8f9ed4121df6a66c0606b9c37c
dave-hickman <dave.robert.hickman@gmail.com>
2023-05-09 15:20:22 +0100

    amendments to 3.5/api

--- __tests__/endpoints.test.js
+++ __tests__/endpoints.test.js
@@ -3,6 +3,7 @@ const request = require('supertest')
 const db = require('../db/connection')
 const {articleData, commentData, topicData, userData} = require('../db/data/test-data/index')
 const seed = require('../db/seeds/seed')
+const JSONendpoints = require('../endpoints.json')
 
 beforeEach(() => {
     return seed({articleData, commentData, topicData, userData})
@@ -17,33 +18,16 @@ describe('GET /api', () => {
         return request(app).get('/api')
         .expect(200)
         .then((response) => {
-            console.log(response.text)
             expect(JSON.parse(response.text)).toBeInstanceOf(Object)
         })
         
     });
-    it('should contain GET /api key, api/topics and api/articles keys', () => {
+    it('should return the same data as the original JSON file', () => {
         return request(app).get('/api')
         .expect(200)
         .then((response) => {
             const parsedEndpoints = JSON.parse(response.text)
-            expect(parsedEndpoints.hasOwnProperty('GET /api')).toBe(true)
-            expect(parsedEndpoints.hasOwnProperty('GET /api/topics')).toBe(true)
-            expect(parsedEndpoints.hasOwnProperty('GET /api/articles')).toBe(true)
-        })
-        
-    });
-    it('should contain description queries and exampleResponse for each endpoint', () => {
-        return request(app).get('/api')
-        .expect(200)
-        .then((response) => {
-            const parsedEndpoints = JSON.parse(response.text)
-            for (let endpointKey in parsedEndpoints){
-                const endpoint = parsedEndpoints[endpointKey]
-                expect(endpoint.hasOwnProperty('description')).toBe(true)
-                expect(endpoint.hasOwnProperty('queries')).toBe(true)
-                expect(endpoint.hasOwnProperty('exampleResponse')).toBe(true)
-            }
+            expect(parsedEndpoints).toEqual(JSONendpoints)
         })
         
     });

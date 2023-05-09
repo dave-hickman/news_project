72c27a25e85b0f02d6e3583209730d08578aa7d1
dave-hickman <dave.robert.hickman@gmail.com>
2023-05-09 16:30:14 +0100

    complete 4.api/articles/article_id

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.get('/api/articles/:article_id', getArticle)
 
 app.use((err, req, res, next) => {
         if (err.status && err.msg){
-            res.status(err.status).send({msg: err.msg})
+            res.status(err.status).send(err.msg)
         }
         if (err.code === '22P02'){
             res.status(400).send('Invalid Input!')

21afd820f8ec445aa5523bc47994e50469436324
dave-hickman <dave.robert.hickman@gmail.com>
2023-05-12 15:21:28 +0100

    amend 204 response

--- contollers/articles_controller.js
+++ contollers/articles_controller.js
@@ -49,7 +49,7 @@ exports.deleteComment = (req, res, next) => {
     const commentID = req.params.comment_id
     removeComment(commentID)
     .then(() => {
-        res.status(204).send({})
+        res.status(204).send()
     })
     .catch(next)
 }
